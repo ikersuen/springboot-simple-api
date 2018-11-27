142797edd33668289221ba1fd27cdbb9108519ce
Iker Suen <suenik@oocl.com>
2018-11-27 16:25:53 +0800

    (iker) Created new class Employee

---
+++ src/main/java/com/tw/apistackbase/Employee.java
@@ -0,0 +1,31 @@
+package com.tw.apistackbase;
+
+public class Employee {
+    private final int id;
+    private final String name;
+    private final int age;
+    private final String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Employee(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
